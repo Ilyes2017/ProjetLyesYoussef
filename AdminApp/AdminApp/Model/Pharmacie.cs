7d26abc509ca4be7bee416094fb48daa0317ccfd
youssef <youssef@DESKTOP-1R67OGD>
2017-02-03 07:50:02 -0800

    bd pharmacie

---
+++ AdminApp/AdminApp/Model/Pharmacie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApp.Model
+{
+    class Pharmacie
+    {
+        public String Id { get; set; }
+        public String nom { get; set; }
+        public String adresse { get; set; }
+        public String disponibilite { get; set; }
+        public Double latitude { get; set; }
+        public Double longitude { get; set; }
+
+    
+    }
+}
+        
